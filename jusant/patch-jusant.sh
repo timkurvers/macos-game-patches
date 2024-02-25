b3eeffb3f2512ce14ab5892d9efac1795865b86d
Tim Kurvers <tim@moonsphere.net>
2024-02-26 00:10:36 +0100

    fix(jusant): allow restoring original file + make patch executable

--- jusant/patch-jusant.sh
+++ jusant/patch-jusant.sh
@@ -7,16 +7,6 @@ if [ ! -f "$1" ]; then
   exit 1
 fi
 
-# Verify Jusant binary (prevent double patching)
-hash=$(md5 -q "$1")
-if [ "$hash" != "09516e36c1f7e64b77b60fa382d350a0" ]; then
-  echo "Exiting as this file is not the main Jusant binary or already patched"
-  exit 1
-fi
-
-# Make a copy of the original
-cp "$1" "$1.bak"
-
 # In RHIInit (address 0x39b82a0), the MessageBox is shown and the game exits:
 #
 # > 0x39b8785  e816060100         call    JusantSpecificDX12RequirementsMessageBox
@@ -24,9 +14,21 @@ cp "$1" "$1.bak"
 # > 0x39b878c  e8df9bdffe         call    FPlatformMisc::RequestExitWithStatus
 #
 # By NOPing out both of the above calls, we effectively bypass the check entirely.
+#
 
-printf "\x90\x90\x90\x90\x90" | dd of="$1" bs=1 seek=60524421 conv=notrunc
-printf "\x90\x90\x90\x90\x90" | dd of="$1" bs=1 seek=60524428 conv=notrunc
+hash=$(md5 -q "$1")
 
-echo
-echo "Successfully patched $1"
+if [ "$hash" == "09516e36c1f7e64b77b60fa382d350a0" ]; then
+  printf "\x90\x90\x90\x90\x90" | dd of="$1" bs=1 seek=60524421 conv=notrunc status=none
+  printf "\x90\x90\x90\x90\x90" | dd of="$1" bs=1 seek=60524428 conv=notrunc status=none
+  echo "Successfully patched $1"
+
+elif [ "$hash" == "4d38e275c8526c68947e2e34ed5c44a2" ]; then
+  printf "\xE8\x16\x06\x01\x00" | dd of="$1" bs=1 seek=60524421 conv=notrunc status=none
+  printf "\xE8\xDF\x9B\xDF\xFE" | dd of="$1" bs=1 seek=60524428 conv=notrunc status=none
+  echo "Restored $1"
+
+else
+  echo "Found an unknown version of ASC-Win64-Shipping.exe"
+  exit 1
+fi
