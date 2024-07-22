2f8c3d23a730a742cb3f603f1e283cd40663fe57
Tim Kurvers <tim@moonsphere.net>
2024-07-22 16:50:01 +0200

    fix(jusant): support build 14182642

--- jusant/patch-jusant.sh
+++ jusant/patch-jusant.sh
@@ -7,29 +7,29 @@ if [ ! -f "$1" ]; then
   exit 1
 fi
 
-# In RHIInit (address 0x39b9c80), the MessageBox is shown and the game exits:
+# In RHIInit (file offset 0x47777a0), the MessageBox is shown and the game exits:
 #
-# > 0x39ba165  e816060100         call    JusantSpecificDX12RequirementsMessageBox
-# > 0x39ba16a  b101               mov     cl, 0x1
-# > 0x39ba16c  e8df9bdffe         call    FPlatformMisc::RequestExitWithStatus
+# > 0x4777c1f  e84c8b0300         call    JusantSpecificDX12RequirementsMessageBox
+# > 0x4777c24  b101               mov     cl, 0x1
+# > 0x4777c26  e8055ad4fe         call    FPlatformMisc::RequestExitWithStatus
 #
 # By NOPing out both of the above calls, we effectively bypass the check entirely.
 #
 
 hash=$(md5 -q "$1")
 
-# Jusant build 13334122
-if [ "$hash" == "3b45cc5aae7d65ecbfe24b3c25fed3f1" ]; then
-  printf "\x90\x90\x90\x90\x90" | dd of="$1" bs=1 seek=$((0x39BA165)) conv=notrunc status=none
-  printf "\x90\x90\x90\x90\x90" | dd of="$1" bs=1 seek=$((0x39BA16C)) conv=notrunc status=none
+# Jusant build 14182642
+if [ "$hash" == "139820f43fd68aeeee66ac1cb3bdd7d0" ]; then
+  printf "\x90\x90\x90\x90\x90" | dd of="$1" bs=1 seek=$((0x4777c1f)) conv=notrunc status=none
+  printf "\x90\x90\x90\x90\x90" | dd of="$1" bs=1 seek=$((0x4777c26)) conv=notrunc status=none
   echo "Successfully patched $1"
 
-elif [ "$hash" == "1028b1a7a2824c5b728d3f95258377c0" ]; then
-  printf "\xE8\x16\x06\x01\x00" | dd of="$1" bs=1 seek=$((0x39BA165)) conv=notrunc status=none
-  printf "\xE8\x1F\x9D\xDF\xFE" | dd of="$1" bs=1 seek=$((0x39BA16C)) conv=notrunc status=none
+elif [ "$hash" == "1c07bd1165eb4865563169807e095d02" ]; then
+  printf "\xe8\x4c\x8b\x03\x00" | dd of="$1" bs=1 seek=$((0x4777c1f)) conv=notrunc status=none
+  printf "\xe8\x05\x5a\xd4\xfe" | dd of="$1" bs=1 seek=$((0x4777c26)) conv=notrunc status=none
   echo "Restored $1"
 
 else
-  echo "Found an unknown version of ASC-Win64-Shipping.exe"
+  echo "Found an unknown version of ASC-Win64-Shipping.exe (hash: $hash)"
   exit 1
 fi
